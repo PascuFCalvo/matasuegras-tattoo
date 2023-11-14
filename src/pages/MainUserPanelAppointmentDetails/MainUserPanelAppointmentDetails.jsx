aef8c5aed97d16a42c7056278e398833b947b6b6
PascuFCalvo <pascual.fernandez.calvo@gmail.com>
2023-11-14 21:01:17 +0100

    feat:startng detailed cards

---
+++ src/pages/MainUserPanelAppointmentDetails/MainUserPanelAppointmentDetails.jsx
@@ -0,0 +1,12 @@
+import "./MainUserPanelAppointmentsDetails.css"
+
+
+export const MainUserPanelAppointmentsDetails = () => {
+   
+
+   return (
+      <>
+      <div className  = "detailedAppointmentCards"></div>
+      </>
+   )
+}
